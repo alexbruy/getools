3c4685dd8b22f4ff39ac19d3bf43c9200233beab
Alexander Bruy <alexander.bruy@gmail.com>
2019-05-08 11:05:20 +0300

    i18n

--- getools.pro
+++ getools.pro
@@ -1,7 +1,14 @@
 SOURCES = __init__.py \
-          getools.py \
-          clicktool.py
+          getoolsplugin.py \
+          kmlwritertask.py \
+          geutils.py \
+          styleutils.py \
+          gui/aboutdialog.py \
+          gui/maptoolclick.py \
+          gui/maptoolselect.py \
+          gui/optionsdialog.py
 
-FORMS = ui/aboutdialogbase.ui
+FORMS = ui/aboutdialogbase.ui \
+        ui/optionsdialogbase.ui
 
 TRANSLATIONS = i18n/getools_uk.ts
---
+++ i18n/getools_uk.ts
@@ -0,0 +1,330 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="../styleutils.py" line="127"/>
+        <source>&quot;{}&quot; renderer is not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../styleutils.py" line="251"/>
+        <source>Symbol layer &quot;{}&quot; is not supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="../gui/aboutdialog.py" line="57"/>
+        <source>About {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/aboutdialog.py" line="61"/>
+        <source>Version: {}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/aboutdialog.py" line="81"/>
+        <source>&lt;p&gt;{description}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: {developer}&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;{homepage}&quot;&gt;{homepage}&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Please report bugs at &lt;a href=&quot;{bugtracker}&quot;&gt;bugtracker&lt;/a&gt;.&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../ui/aboutdialogbase.ui" line="40"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/aboutdialogbase.ui" line="63"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Sans Serif&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GeToolsPlugin</name>
+    <message>
+        <location filename="../getoolsplugin.py" line="60"/>
+        <source>Coords to the Google Earth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../getoolsplugin.py" line="65"/>
+        <source>Feature(s) to the Google Earth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../getoolsplugin.py" line="70"/>
+        <source>Vector layer to the Google Earth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../getoolsplugin.py" line="74"/>
+        <source>Raster layer to the Google Earth</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../getoolsplugin.py" line="78"/>
+        <source>Settings&#xe2;&#x80;&#xa6;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../getoolsplugin.py" line="82"/>
+        <source>About GETools&#xe2;&#x80;&#xa6;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../getoolsplugin.py" line="198"/>
+        <source>GETools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../getoolsplugin.py" line="194"/>
+        <source>Successfully exported to &lt;a href=&quot;{}&quot;&gt;{}&lt;/a&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>KmlWriterTask</name>
+    <message>
+        <location filename="../kmlwritertask.py" line="62"/>
+        <source>position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmlwritertask.py" line="119"/>
+        <source>Currently only GDAL rasters are supported.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmlwritertask.py" line="173"/>
+        <source>Unsupported geometry type &quot;{}&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmlwritertask.py" line="187"/>
+        <source>Failed to export layer &quot;{layer}&quot;: Cannot set pipe provider.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmlwritertask.py" line="201"/>
+        <source>Failed to export layer &quot;{layer}&quot;: Cannot set pipe nuller.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../kmlwritertask.py" line="211"/>
+        <source>Failed to export layer &quot;{layer}&quot;: {message}.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OptionsDialog</name>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="20"/>
+        <source>GETools options</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="77"/>
+        <source>Points</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="79"/>
+        <source>Lines</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="81"/>
+        <source>Polygons</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="83"/>
+        <source>Labels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="85"/>
+        <source>Rasters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="633"/>
+        <source>Attributes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="639"/>
+        <source>Field with feature name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="649"/>
+        <source>Field with feature description</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="1018"/>
+        <source>Style</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="914"/>
+        <source>Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="931"/>
+        <source>Color mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="941"/>
+        <source>Scale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="1049"/>
+        <source>Geometry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="1064"/>
+        <source>Altitude mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="1071"/>
+        <source> m</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="793"/>
+        <source>Altitude above sea level</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="347"/>
+        <source>Connect point to the ground</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="491"/>
+        <source>Width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="758"/>
+        <source> px</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="559"/>
+        <source>Connect line to the ground</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="566"/>
+        <source>Allow line to follow the terrain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="683"/>
+        <source>Stroke color mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="690"/>
+        <source>Fill color mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="697"/>
+        <source>Fill color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="717"/>
+        <source>Fill polygon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="724"/>
+        <source>Outline polygon</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="731"/>
+        <source>Stroke color</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="738"/>
+        <source>Stroke width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="819"/>
+        <source>Connect polygon to the ground</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="826"/>
+        <source>Allow polygon to follow the terrain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="1039"/>
+        <source>Use rendered image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ui/optionsdialogbase.ui" line="1087"/>
+        <source>Altitude above surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="90"/>
+        <source>Normal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="90"/>
+        <source>Random</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="100"/>
+        <source>Clamp to ground</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="100"/>
+        <source>Relative to ground</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="100"/>
+        <source>Absolute</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="100"/>
+        <source>Relative to sea floor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/optionsdialog.py" line="100"/>
+        <source>Clamp to sea floor</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
